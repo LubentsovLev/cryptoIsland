87bd565e30bd7c6f3274fd659ead3a01a8d8bcba
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2020-12-12 16:10:13 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,88 @@
-$(function () {});
+$(function () {
+  $('.up__items').slick({
+    infinite: false,
+    slidesToShow: 3.3,
+    slidesToScroll: 0.6,
+    responsive: [
+      {
+        breakpoint: 1160,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2.5,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2.6,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 690,
+        settings: {
+          slidesToShow: 2.6,
+          slidesToScroll: 0.7,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2.2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  if (document.documentElement.clientWidth < 1160) {
+    $('.make__items').slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1160,
+          settings: {
+            slidesToScroll: 3,
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2.6,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 690,
+          settings: {
+            slidesToShow: 2.6,
+            slidesToScroll: 0.7,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2.7,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+    $('.growth__items').slick({
+      infinite: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: 'unslick',
+        },
+      ],
+    });
+  }
+});
 
 let burger = document.querySelector('.burger__burger');
 let nav = document.querySelector('.firstMain__header-items');
@@ -14,7 +98,7 @@ function closePop(popup) {
 }
 
 burger.addEventListener('click', function () {
-  this.classList.toggle('active');
+  this.classList.toggle('burger_active');
   nav.classList.toggle('nav_active');
 });
 document.addEventListener('click', function (e) {
@@ -32,9 +116,27 @@ document.addEventListener('click', function (e) {
     toggleOwerflow('visible');
   }
   if (e.target.classList.contains('open_popup-auth')) {
-    burger.classList.remove('active');
+    burger.classList.remove('burger_active');
     nav.classList.remove('nav_active');
     popup__auth.classList.add('popup__active');
     toggleOwerflow('hidden');
+  }
+});
+
+document.addEventListener('click', function (e) {
+  let all = document.querySelectorAll('.docs__item');
+  let allPl = document.querySelectorAll('.docs-pl');
+
+  if (e.target.classList.contains('docs-pl')) {
+    for (let i = 0; i < all.length; i++) {
+      all[i].classList.remove('docs__active');
+      allPl[i].innerHTML = '+';
+    }
+    if (e.target.innerHTML === '+') {
+      e.target.parentElement.parentElement.classList.add('docs__active');
+    } else {
+      e.target.parentElement.parentElement.classList.remove('docs__active');
+    }
+    e.target.innerHTML = e.target.innerHTML === '+' ? '-' : '+';
   }
 });

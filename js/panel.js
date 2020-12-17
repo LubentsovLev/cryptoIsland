cd4ab44b8a302735b86213943cf6429b98239a3c
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2020-12-17 10:33:02 +0300

    added links : firstBlock , secondBlock

--- js/panel.js
+++ js/panel.js
@@ -3,10 +3,36 @@ $(function () {});
 let burger = document.querySelector('.burger__burger');
 let nav = document.querySelector('.panel__main-menu');
 
-let popup__reg = document.querySelector('.popup__reg');
-let popup__auth = document.querySelector('.popup__auth');
+let popup__wallet = document.querySelector('.popup__wallet');
 burger.addEventListener('click', function () {
   this.classList.toggle('active');
   nav.classList.toggle('nav_active');
   document.body.classList.toggle('dis');
+});
+
+function toggleOwerflow(style) {
+  document.body.style.overflow = style;
+}
+function closePop(popup) {
+  popup.classList.remove('popup__active');
+}
+
+// burger.addEventListener('click', function () {
+//   this.classList.toggle('burger_active');
+//   nav.classList.toggle('nav_active');
+// });
+
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('open_popup-wallet')) {
+    closePop(popup__wallet);
+    popup__wallet.classList.add('popup__active');
+    toggleOwerflow('hidden');
+  }
+  if (
+    e.target.classList.contains('popup__x') ||
+    e.target.classList.contains('popup')
+  ) {
+    popup__wallet.classList.remove('popup__active');
+    toggleOwerflow('visible');
+  }
 });
